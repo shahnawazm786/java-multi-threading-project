1896abd79230816b2001e89dbadbc80ba41763c5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-02-23 12:24:11 +0530

    Muulti Threading

---
+++ src/main/java/org/mutlithreading/SupervisorExample.java
@@ -0,0 +1,26 @@
+package org.mutlithreading;
+
+public class SupervisorExample {
+    public static void main(String[] args) throws InterruptedException {
+    Worker1 worker1=new Worker1();
+    Worker2 worker2=new Worker2();
+    worker1.executeWork();
+    worker2.executeWork();
+    }
+}
+class Worker1{
+    public void executeWork() throws InterruptedException{
+        for(int i=0;i<=10;i++){
+            Thread.sleep(100);
+            System.out.println("Worker 1 is executed task :"+i);
+        }
+    }
+}
+class Worker2{
+    public void executeWork() throws InterruptedException{
+        for(int i=0;i<=10;i++){
+            Thread.sleep(100);
+            System.out.println("Worker 2 is executed task :"+i);
+        }
+    }
+}
