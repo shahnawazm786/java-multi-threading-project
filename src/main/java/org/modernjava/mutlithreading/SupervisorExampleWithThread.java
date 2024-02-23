cf98a8fdf289815dad943508766203641bc58be9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-02-23 13:47:32 +0530

    Muulti Threading

---
+++ src/main/java/org/modernjava/mutlithreading/SupervisorExampleWithThread.java
@@ -0,0 +1,13 @@
+package org.modernjava.mutlithreading;
+
+public class SupervisorExampleWithThread {
+    public static void main(String[] args) {
+
+    }
+}
+class ParallelWorker1 extends Thread{
+
+}
+class ParallelWorker2 extends Thread{
+
+}
